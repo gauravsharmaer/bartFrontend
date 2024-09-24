00eb7a6a25075342653152de48fffc41e416d207
gauravsharmaer <gauravsharma701998@gmail.com>
2024-09-24 16:52:23 +0530

    bart input box

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "../pages/signup/Signup";
 import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,82 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
+
+// import * as React from "react";
+
+// export interface InputProps
+//   extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+// const Input = React.forwardRef<HTMLInputElement, InputProps>(
+//   ({ className = "", type, ...props }, ref) => {
+//     return (
+//       <input
+//         type={type}
+//         className={`h-[58px] w-[360px] rounded-full p-7 border-none border-1  border-[#282829] bg-[#1D1D1D]
+//           text-[#AAAAAA] ${className}`}
+//         ref={ref}
+//         {...props}
+//       />
+//     );
+//   }
+// );
+
+// Input.displayName = "Input";
+
+// export { Input };
+
 import * as React from "react";
+import { useState, useRef, useEffect } from "react";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className = "", type, ...props }, ref) => {
+  ({ className = "", type, placeholder = "", ...props }, ref) => {
+    const [isFilled, setIsFilled] = useState(false);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+      if (ref && typeof ref === "function") {
+        ref(inputRef.current);
+      } else if (ref) {
+        (ref as React.MutableRefObject<HTMLInputElement | null>).current =
+          inputRef.current;
+      }
+    }, [ref]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setIsFilled(!!e.target.value); // Check if input has content
+      if (props.onChange) props.onChange(e); // Pass the onChange event if provided
+    };
+
+    const handleLabelClick = () => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    };
+
     return (
-      <input
-        type={type}
-        className={`h-[58px] w-[360px] rounded-full p-7 border-none border-1  border-[#282829] bg-[#1D1D1D]
-          text-[#AAAAAA] ${className}`}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative w-[360px]">
+        <input
+          type={type}
+          className={`h-[58px] w-full rounded-full p-7 border-none border-1  border-[#282829] bg-[#1D1D1D] focus:outline-none
+            text-[#AAAAAA] peer ${className}`}
+          ref={inputRef}
+          placeholder=" " // Empty placeholder to reserve space for the floating label
+          onChange={handleInputChange}
+          {...props}
+        />
+        <label
+          className={`absolute left-7 top-[16%] text-[12px] transform -translate-y-1/2 text-[#AAAAAA] transition-all duration-200 cursor-text
+    ${
+      isFilled
+        ? " text-[#888] text-[12px]"
+        : "peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base left-9"
+    }`}
+          onClick={handleLabelClick}
+        >
+          {placeholder}
+        </label>
+      </div>
     );
   }
 );
